16f8bed248332732aea667674e94537e5bd9fb21
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2023-10-24 19:53:49 +0200

    Initial commit

---
+++ src/test/java/com/healthcare/citasmedicas/CitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.citasmedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
