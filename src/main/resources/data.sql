3a875021f0540b01f7c8f2a0520535da085e2d32
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2023-10-24 21:04:30 +0200

    24/10/2023
    Acceso a Datos:
    
    //Crear citas-medicas project

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,16 @@
-DROP TABLE IF EXISTS Data;
+DROP SCHEMA IF EXISTS datos;
+CREATE DATABASE datos;
 
-USE Data;
-
-CREATE TABLE RESERVATION (
+USE datos;
+CREATE TABLE RESERVAS (
 id INT AUTO_INCREMENT PRIMARY KEY,
-reservation_date DATE NULL DEFAULT NULL,
-name VARCHAR(250) NULL,
-status VARCHAR(25) NOT NULL DEFAULT 'FREE'
+fecha TIMESTAMP NULL DEFAULT NULL,
+nombre_paciente VARCHAR(250) NULL,
+estado VARCHAR(25) NOT NULL DEFAULT 'FREE'
 );
 
-INSERT INTO RESERVATION (fecha, nombre_paciente, estado) VALUES ('2023-11-20 10:00:00', 'Alejandro Ortega', 'Pendiente');
-INSERT INTO RESERVATION (fecha, nombre_paciente, estado) VALUES ('2023-11-20 11:00:00', 'Isabel Plaza', 'Confirmada');
+INSERT INTO RESERVAS (fecha, nombre_paciente, estado) VALUES ('2023-11-20 10:00:00', 'Alejandro Ortega', 'Pendiente');
+INSERT INTO RESERVAS (fecha, nombre_paciente, estado) VALUES ('2023-11-20 11:00:00', 'Isabel Plaza', 'Confirmada');
+
+
+SELECT *  FROM RESERVAS;
