93fbfd25f78fd784637ea3a6b9fa14b677f4a3de
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2023-10-25 04:34:10 +0200

    25/10/2023
    Acceso a Datos:
    
    //Actividades Tema 4 y 5. (Creado el "programilla" del punto 5 en CitasMedicasApplication.java (main).

---
+++ src/main/java/com/healthcare/citasmedicas/Citas.java
@@ -0,0 +1,268 @@
+package com.healthcare.citasmedicas;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Citas {
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL_CONEXION = "jdbc:mysql://localhost:3306/datos";
+    private static final String usuario = "root";
+    private static final String password = "1234";
+	int id;
+	Date fecha;
+	String nombrePaciente;
+	String estado;
+	Connection dbConnection = null;
+    java.sql.Statement statement = null;
+    Connection connection = null;
+    PreparedStatement preparedStatement=null;
+    
+	public Citas() {
+
+	}
+
+	public Citas(int id, Date fecha, String nombrePaciente, String estado) {
+
+		this.id = id;
+		this.fecha = fecha;
+		this.nombrePaciente = nombrePaciente;
+		this.estado = estado;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getNombrePaciente() {
+		return nombrePaciente;
+	}
+
+	public void setNombrePaciente(String nombrePaciente) {
+		this.nombrePaciente = nombrePaciente;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	// Métodos específicos de clase Citas (Listar, insertar, eliminar y actualizar datos en la tabla)
+	public void listarRegistro() {
+    try {
+        Class.forName(DRIVER);
+        dbConnection = DriverManager.getConnection(URL_CONEXION, usuario, password);
+        String selectTableSQL = "SELECT * FROM RESERVAS";
+        statement = dbConnection.createStatement();
+        ResultSet resultado = statement.executeQuery(selectTableSQL);
+        System.out.println("LISTA DE PACIENTES:" +
+        		 "\n");
+        while (resultado.next()) {
+            setId(resultado.getInt("ID"));
+            setFecha(resultado.getDate("FECHA"));
+            setNombrePaciente(resultado.getString("NOMBRE_PACIENTE"));
+            setEstado(resultado.getString("ESTADO"));
+           
+            System.out.println("ID: " + getId() + "\n"
+    				+ "Fecha cita: " + getFecha() + "\n"
+					+ "Nombre del paciente: " + getNombrePaciente() + "\n"
+					+ "Estado de la cita: " + getEstado() + "\n");
+
+        }
+    } catch (SQLException e) {
+        System.out.println(e.getMessage());
+    } catch (ClassNotFoundException e) {
+        System.out.println(e.getMessage());
+    } finally {
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+            if (dbConnection != null) {
+                dbConnection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+	
+}
+	//INSERTAR REGISTRO DE CITA
+	public String insertarRegistro(Date fechaIns, String nombre_pacienteIns, String estado_citaIns) {
+	    Citas citaIngresada = new Citas();
+		
+	    citaIngresada.setFecha(fechaIns);
+	    citaIngresada.setNombrePaciente(nombre_pacienteIns);
+	    citaIngresada.setEstado(estado_citaIns);
+		
+	    try {
+	        Class.forName(DRIVER);
+	        connection = DriverManager.getConnection(URL_CONEXION, usuario, password);
+	        String insertTableSQL = "INSERT INTO RESERVAS VALUES (ID, ?, ?, ?)";
+	        
+	        
+	        preparedStatement = connection.prepareStatement(insertTableSQL);
+	      
+	        preparedStatement.setDate(1, citaIngresada.getFecha());
+	        preparedStatement.setString(2, citaIngresada.getNombrePaciente());
+	        preparedStatement.setNString(3, citaIngresada.getEstado());
+	        
+      
+	        int filasAfectadas = preparedStatement.executeUpdate();
+
+	        //Verificamos si la consulta SQL se ejecuta correctamente
+	        if (filasAfectadas > 0){
+	            System.out.println("Se ha ejecutado correctamente la secuencia SQL. Total de filas afectadas: " + filasAfectadas);
+	        }
+	        else{
+	            System.out.println("No se ha modificado ninguna fila de la BBDD.");
+	        }
+	        
+	    } catch (SQLException e) {
+	        System.out.println(e.getMessage());
+	    } catch (ClassNotFoundException e) {
+	        System.out.println(e.getMessage());
+	    } finally {
+	        try {
+	            if (preparedStatement != null) {
+	            	preparedStatement.close();
+	            }
+	            if (connection != null) {
+	                connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	        }
+	    }
+		return "La siguiente cita ha sido registrada satisfactoriamente: \n" + citaIngresada.toString();
+		
+	}
+//ELIMINAR REGISTRO DE CITA
+	public String eliminarRegistro(int IDIns) {
+	    Citas citaIngresada = new Citas();
+		
+	    citaIngresada.setId(IDIns);
+	
+		
+	    try {
+	        Class.forName(DRIVER);
+	        connection = DriverManager.getConnection(URL_CONEXION, usuario, password);
+	        String insertTableSQL = "DELETE FROM RESERVAS WHERE ID=?";
+	        
+	        
+	        preparedStatement = connection.prepareStatement(insertTableSQL);
+	      
+	        preparedStatement.setInt(1, citaIngresada.getId());
+
+	        
+      
+	        int filasAfectadas = preparedStatement.executeUpdate();
+
+	        //Verificamos si la consulta SQL se ejecuta correctamente
+	        if (filasAfectadas > 0){
+	            System.out.println("Se ha ejecutado correctamente la secuencia SQL. Total de filas afectadas: " + filasAfectadas);
+	        }
+	        else{
+	            System.out.println("No se ha modificado ninguna fila de la BBDD.");
+	        }
+	        
+	    } catch (SQLException e) {
+	        System.out.println(e.getMessage());
+	    } catch (ClassNotFoundException e) {
+	        System.out.println(e.getMessage());
+	    } finally {
+	        try {
+	            if (preparedStatement != null) {
+	            	preparedStatement.close();
+	            }
+	            if (connection != null) {
+	                connection.close();
+	            }
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	        }
+	    }
+		return "La cita con ID: " + citaIngresada.getId() + " ha sido eliminada del registro. \n";
+		
+	}
+	
+	
+	//ACTUALIZAR REGISTROS DE CITA
+		public String modificarRegistro(int IDIns, Date fechaIns , String nombre_pacienteIns, String estado_citaIns) {
+		    Citas citaIngresada = new Citas();
+			
+		    citaIngresada.setId(IDIns);
+		    citaIngresada.setFecha(fechaIns);
+		    citaIngresada.setNombrePaciente(nombre_pacienteIns);
+		    citaIngresada.setEstado(estado_citaIns);
+			
+		    try {
+		        Class.forName(DRIVER);
+		        connection = DriverManager.getConnection(URL_CONEXION, usuario, password);
+		        String insertTableSQL = "UPDATE RESERVAS SET FECHA = ?, NOMBRE_PACIENTE = ?, ESTADO= ? WHERE ID = ?";
+		        
+		        
+		        preparedStatement = connection.prepareStatement(insertTableSQL);
+		      
+		        preparedStatement.setDate(2, citaIngresada.getFecha());
+		        preparedStatement.setString(3, citaIngresada.getNombrePaciente());
+		        preparedStatement.setNString(4, citaIngresada.getEstado());
+		        
+	      
+		        int filasAfectadas = preparedStatement.executeUpdate();
+
+		        //Verificamos si la consulta SQL se ejecuta correctamente
+		        if (filasAfectadas > 0){
+		            System.out.println("Se ha ejecutado correctamente la secuencia SQL. Total de filas afectadas: " + filasAfectadas);
+		        }
+		        else{
+		            System.out.println("No se ha modificado ninguna fila de la BBDD.");
+		        }
+		        
+		    } catch (SQLException e) {
+		        System.out.println(e.getMessage());
+		    } catch (ClassNotFoundException e) {
+		        System.out.println(e.getMessage());
+		    } finally {
+		        try {
+		            if (preparedStatement != null) {
+		            	preparedStatement.close();
+		            }
+		            if (connection != null) {
+		                connection.close();
+		            }
+		        } catch (SQLException e) {
+		            System.out.println(e.getMessage());
+		        }
+		    }
+			return "La cita con ID: " + citaIngresada.getId() + " ha sido actualizada con nuevos datos. \n";
+			
+		}
+	
+	@Override
+	public String toString() {
+		return "ID : "+ id + "\n"
+				+ "Fecha cita: " + fecha + "\n"
+						+ "Nombre del paciente: " + nombrePaciente + "\n"
+								+ "Estado de la cita: " + estado + "\n";
+	}
+
+}
--- src/main/java/com/healthcare/citasmedicas/CitasMedicasApplication.java
+++ src/main/java/com/healthcare/citasmedicas/CitasMedicasApplication.java
@@ -1,6 +1,8 @@
 package com.healthcare.citasmedicas;
 
-import java.sql.Date;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,68 +11,107 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CitasMedicasApplication {
 
 	public static void main(String[] args) {
+		final String ANSI_RED = "\u001B[31m";
+		final String ANSI_GREEN = "\u001B[32m";
+		final String ANSI_UNDERLINE = "\u001B[4m";
+		final String ANSI_RESET = "\u001B[0m";
 		SpringApplication.run(CitasMedicasApplication.class, args);
-	
-	
-	
+		Citas patientCheck = new Citas();
+		Scanner sc = new Scanner(System.in);
+		String cleaningCache="";
+		String menu;
+		int IDIns = 0;
+		String fechaString;
+		java.sql.Date fecha = null;
+		String nombre="";
+		String estado="";
+		boolean salir = true;
+
+		while (salir) {
+			System.out.println(ANSI_GREEN + ANSI_UNDERLINE + "Menu de Health Care:" + ANSI_RESET + "\n"
+					+ "1.- LISTAR CITAS DE PACIENTES" + "\n" + "2.- AGREGAR CITA NUEVA" + "\n"
+					+ "3.- ELIMINAR REGISTRO DE CITAS" + "\n" + "4.- ACTUALIZAR REGISTRO DE CITAS" + "\n"
+					+ "5.- SALIR DEL PROGRAMA" + "\n" + "Escriba un numero del 1 al 5:");
+			menu = sc.nextLine();
+
+			switch (menu) {
+			case "1":
+				patientCheck.listarRegistro();
+				break;
+
+			case "2":
+
+				System.out.print("Ingrese una fecha (en formato yyyy-MM-dd): ");
+				fechaString = sc.nextLine();
+				System.out.println("Ahora escriba el nombre completo del paciente: ");
+				nombre = sc.nextLine();
+				System.out.println("Por ultimo, establezca un estado para su cita (Pendiente, Confirmada, Cancelada): ");
+				estado = sc.nextLine();
+
+				try {
+					SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+					java.util.Date parsedDate = dateFormat.parse(fechaString);
+					fecha = new java.sql.Date(parsedDate.getTime());
+
+				} catch (ParseException e) {
+					System.out.println("Error al convertir fecha");
+				}
+
+				patientCheck.insertarRegistro(fecha, nombre, estado);
+
+				break;
+
+			case "3":
+
+				try {
+					System.out.print("Para eliminar un registro inserte el ID de la cita: ");
+					IDIns = sc.nextInt();
+					cleaningCache=sc.nextLine();
+				} catch (NumberFormatException e) {
+					System.out.println("Por favor, inserte un valor numerico entero para el ID.");
+				}
+
+				patientCheck.eliminarRegistro(IDIns);
+				IDIns = 0;
+				break;
+
+			case "4":
+				try {
+					System.out.print("Para actualizar un registro inserte el ID de la cita a modificar: ");
+					IDIns = sc.nextInt();
+					cleaningCache=sc.nextLine();
+					System.out.print("Ingrese una fecha (en formato yyyy-MM-dd): ");
+					fechaString = sc.nextLine();
+					System.out.println("Ahora escriba el nombre completo del paciente: ");
+					nombre = sc.nextLine();
+					System.out.println("Por ultimo, establezca un estado para su cita (Pendiente, Confirmada, Cancelada):");
+					estado = sc.nextLine();
+
+					SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+					java.util.Date parsedDate = dateFormat.parse(fechaString);
+					fecha = new java.sql.Date(parsedDate.getTime());
+
+				} catch (NumberFormatException e) {
+					System.out.println("Por favor, inserte un valor numerico entero para el ID.");
+				} catch (ParseException e) {
+					System.out.println("Error al convertir fecha");
+				}
+				
+				patientCheck.modificarRegistro(IDIns, fecha, nombre, estado);
+				IDIns = 0;
+				break;
+
+			case "5":
+				salir = false;
+				System.out.println(ANSI_RED + "Saliendo del sistema, espere por favor." + ANSI_RESET);
+				sc.close();
+				break;
+
+			default:
+				break;
+			}
+		}
+// Obtener datos de los usuarios:
+
 	}
-	// Alejandro Ortega Maldonado
-	public class Citas {
-		 int id;
-		 Date fecha;
-		 String nombrePaciente;
-		 String estado;
-		
-
-		public Citas(int id, Date fecha, String nombrePaciente, String estado) {
-			
-			this.id = id;
-			this.fecha = fecha;
-			this.nombrePaciente = nombrePaciente;
-			this.estado = estado;
-		}
-
-
-		public int getId() {
-			return id;
-		}
-
-
-		public void setId(int id) {
-			this.id = id;
-		}
-
-
-		public Date getFecha() {
-			return fecha;
-		}
-
-
-		public void setFecha(Date fecha) {
-			this.fecha = fecha;
-		}
-
-
-		public String getNombrePaciente() {
-			return nombrePaciente;
-		}
-
-
-		public void setNombrePaciente(String nombrePaciente) {
-			this.nombrePaciente = nombrePaciente;
-		}
-
-
-		public String getEstado() {
-			return estado;
-		}
-
-
-		public void setEstado(String estado) {
-			this.estado = estado;
-		}
-		
-		
-	}
-	
 }
